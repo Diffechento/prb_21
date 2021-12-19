5cdfed3d4d2ade5b9b4aa0e9b4689177af8e671f
Diffechento <95750109+Diffechento@users.noreply.github.com>
2021-12-19 19:25:58 +0300

    Create Task 3.py

---
+++ Lesson 1/Task 3.py
@@ -0,0 +1,8 @@
+def string_lenght(string):
+    return (len(set(string)))  # С помощью функции set удаляем из строки повторяющиеся символы,функцией len получаем длинну полученной строки и возвращаем значение
+
+
+if __name__ == '__main__':
+    input_string = input()  # Вводим строку с клавиатуры
+    print(string_lenght(input_string)) # Выводим значение функции
+
