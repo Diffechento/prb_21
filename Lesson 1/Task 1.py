ed5ea647b0149b1ea0298de8ec5504820ba12380
Diffechento <95750109+Diffechento@users.noreply.github.com>
2021-12-19 18:57:23 +0300

    Create Task 1.py

---
+++ Lesson 1/Task 1.py
@@ -0,0 +1,6 @@
+def main():
+    input_string = input() # Вводим строку с клавиатуры
+    print(len(input_string)) # С помощью функции len получаем длинну полученной строки и выводим
+
+if __name__ == '__main__':
+    main()
